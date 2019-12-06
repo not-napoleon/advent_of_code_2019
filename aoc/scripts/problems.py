fc977419e768d654ff8623e9f58bde5f0f399dd1
Mark Tozzi <mark.tozzi@gmail.com>
2019-12-05 22:12:47 -0500

    Day 2 part 1

--- aoc/scripts/problems.py
+++ aoc/scripts/problems.py
@@ -23,4 +23,26 @@ def fuel(data, recurse):
                 fuel_increment = (fuel_increment // 3) -2
         else:
             total += fuel_increment
-    print("Total is %s" % total)
+    click.echo("Total is %s" % total)
+
+@cli.command()
+@click.argument('data', type=click.File('r'))
+def intcode(data):
+    raw = data.read().replace("\n", "")
+    program = [int(value) for value in raw.split(",")]
+    op_ptr = 0
+    while program[op_ptr] != 99:
+        (op, in_1, in_2, out) = program[op_ptr:op_ptr+4]
+        if op == 1:
+            program[out] = program[in_1] + program[in_2]
+        elif op == 2:
+            program[out] = program[in_1] * program[in_2]
+        elif op == 99:
+            # not sure how this happened, but fine
+            break
+        else:
+            click.echo("Invalid op at %s, current state is \n%s" % (op_ptr, program))
+            exit(1)
+        op_ptr += 4
+    click.echo("Position 0: %s" % program[0])
+    click.echo("Final state: %s" % program)
