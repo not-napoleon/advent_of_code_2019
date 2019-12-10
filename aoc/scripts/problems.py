b4d727fb521398090a8ddb98ac454208bfc0967a
Mark Tozzi <mark.tozzi@gmail.com>
2019-12-09 21:38:40 -0500

    Day 3

--- aoc/scripts/problems.py
+++ aoc/scripts/problems.py
@@ -60,3 +60,52 @@ def intcode(data):
             if output == 19690720:
                 break
     click.echo("noun: %s, verb: %s, answer: %s" % (i, j, 100 * i + j))
+
+def process_row(row):
+    moves = row.split(",")
+    current_point = (0,0,)
+    points = {}
+    steps = 0
+    for move in moves:
+        (direction, count) = (move[0], int(str(move[1:])))
+        for i in range(count):
+            steps += 1
+            if direction == "U":
+                new_point = (current_point[0], current_point[1]+1)
+            elif direction == "D":
+                new_point = (current_point[0], current_point[1]-1)
+            elif direction == "L":
+                new_point = (current_point[0]-1, current_point[1])
+            elif direction == "R":
+                new_point = (current_point[0]+1, current_point[1])
+            else:
+                click.echo("Unknown direction %s" % direction)
+                exit(1)
+            current_point = new_point
+            if current_point not in points:
+                # good enough, since steps is monotonically increasing
+                points[current_point] = steps
+    return points
+
+@cli.command()
+@click.argument('data', type=click.File('r'))
+@click.option("--dist", "taxi_mode", flag_value=True)
+@click.option("--score", "taxi_mode", flag_value=False)
+def paths(data, taxi_mode):
+    rows = data.readlines()
+    path1 = process_row(rows[0])
+    path2 = process_row(rows[1])
+    if len(path2) > len(path1):
+        (path1, path2) = (path2, path1)
+    best_dist = None
+    for point in path1.keys():
+        if point in path2:
+            if taxi_mode:
+                dist = abs(point[0]) + abs(point[1])
+            else:
+                dist = path1[point] + path2[point]
+            if best_dist is None:
+                best_dist = dist
+            else:
+                best_dist = min(best_dist, dist)
+    click.echo("Shortest distance to interection: %s" % best_dist)
